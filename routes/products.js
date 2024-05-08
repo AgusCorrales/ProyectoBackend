3775f9f900ec42c11ae46da903d47886fa4368f7
AgusCorrales <aguscorrales1997@gmail.com>
2024-05-08 11:39:46 +0200

    Actualizacion de productos

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,14 @@
- const ProductController = require("../controllers/ProductController");
- const express = require("express");
+const ProductController = require("../controllers/ProductController");
+const express = require("express");
 const UserController = require("../controllers/UserController");
- const router = express.Router();
+const router = express.Router();
+const {authentication} = require('../middleware/authentication')
 
 
-
-router.post("/",ProductController.create);
+router.post("/",authentication,ProductController.create);
 router.get("/",ProductController.getAll);
-router.put("/id/:id",ProductController.update);
-router.delete("/id/:id",ProductController.delete);
+router.put("/id/:id",authentication,ProductController.update);
+router.delete("/id/:id",authentication,ProductController.delete);
 router.get("/id/:id",ProductController.getById);
 router.get("/name/:name",ProductController.productFilter);
 router.get("/price/:price",ProductController.getByPrice);
