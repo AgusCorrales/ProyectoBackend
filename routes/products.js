8ff8d0659dd6db58e0d6d3c8b9b07f0e36bb546e
AgusCorrales <aguscorrales1997@gmail.com>
2024-05-08 11:50:17 +0200

    Actualizacion product

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,5 @@
 const ProductController = require("../controllers/ProductController");
 const express = require("express");
-const UserController = require("../controllers/UserController");
 const router = express.Router();
 const {authentication} = require('../middleware/authentication')
 
